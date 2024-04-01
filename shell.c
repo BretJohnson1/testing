f0cf05aeff00b0370220e316384cebae70f139cc
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-01 16:47:03 +0000

    more testing

--- shell.c
+++ shell.c
@@ -16,12 +16,14 @@ int main(int argc, char **argv, char **env)
 	char *args[20], *paths[20];
 	int ret_val = 0;
 	int i;
-
+/** ignore used arguments*/
 	(void) argc;
 	(void) argv;
+/** main shell loop*/
 	while (1)
 	{
 		i = 0;
+/** extract PATH environment variable*/
 		while (env[i])
 		{
 			if (_strncmp(env[i], "PATH=", 5) == 0)
@@ -31,17 +33,21 @@ int main(int argc, char **argv, char **env)
 			}
 			i++;
 		}
+/** tokenize the PATH variable*/
 		tokenize_string(path, ":", paths);
+/** print shell prompt if running interactively*/
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$", 12);
+			write(STDOUT_FILENO, "$uh Dude: ", 11);
+/** read input line*/
 		if (getline(&line, &nread, stdin) == -1)
 		{
 			free(line);
 			free(path);
 			exit(EXIT_SUCCESS);
 		}
+/** tokenize input line*/
 		tokenize_string(line, " \n\t", args);
-
+/** handle built in commands or execute external commands*/
 		if (!args[0])
 		{
 			free(path);
@@ -63,7 +69,7 @@ int main(int argc, char **argv, char **env)
 			free(path);
 			continue;
 		}
-
+/** check if command is executbale*/
 		if (access(args[0], X_OK) == 0)
 			make_a_baby(path, args[0], args);
 		else
@@ -81,15 +87,17 @@ int main(int argc, char **argv, char **env)
  */
 void tokenize_string(char *line, char *delims, char **tokens)
 {
+/** use strtok to tokenize the input line based on delimiters*/
 	char *path_token = strtok(line, delims);
 	int i = 0;
-
+/** iterate through the tokens and store them in the tokens array*/
 	while (path_token != NULL)
 	{
 		tokens[i] = path_token;
 		i++;
 		path_token = strtok(NULL, delims);
 	}
+/** set the last element of the tokens array to NULL to mark the end*/
 	tokens[i] = NULL;
 }
 /**
@@ -104,22 +112,30 @@ int check_path(char *path, char **patharr, char **args)
 	int i = 0;
 	char *comp_path = NULL;
 	struct stat x;
-
+/** loop through each path in the patharr array*/
 	while (patharr[i] != NULL)
 	{
+/** allocate memory for the complate path*/
 		comp_path = malloc(_strlen(args[0]) + _strlen(patharr[i]) + 2);
+/** copy the directory path from patharr[i] to comp_path*/
 		_strcpy(comp_path, patharr[i]);
+/** concatenate '/' to the directory path*/
 		_strcat(comp_path, "/");
+/** concatenate the command name to the complete path*/
 		_strcat(comp_path, args[0]);
+/** check if the complete path exists and is exectuable*/
 		if (stat(comp_path, &x) == 0)
 		{
+/** if the file exists and is exectuable, execute it*/
 			make_a_baby(path, comp_path, args);
 			free(comp_path);
 			return (0);
 		}
+/** free memory allocated for comp_path*/
 		free(comp_path);
 		i++;
 	}
+/** if no executable is found, return 127*/
 	return (127);
 }
 
@@ -137,24 +153,30 @@ int make_a_baby(char *stdpath, char *call_path, char **str_arr)
 	pid_t cop;
 	pid_t sig;
 	int status = 0;
-
+/** free memory allocated for stdpath*/
 	free(stdpath);
+/** fork a new process*/
 	cop = fork();
 	if (cop == 0)
 	{
+/** execute the command with the given path and arguments*/
 		if (execve(call_path, str_arr, NULL) == -1)
+/** exit child process if execution fails*/
 			exit(EXIT_FAILURE);
 	}
 	else if (cop < 0)
 	{
+/** fork failed, exit with failure status*/
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		do {
+/** wait for the child process to terminate or stop*/
 			sig = waitpid(cop, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
+/** return the status of the child process to the caller*/
 	(void) sig;
 	return (status);
 }
@@ -168,28 +190,30 @@ char *_strdup(char *str)
 	int i = 0;
 	int n = 0;
 	char *ar;
-
+/** check if the input string is NULL*/
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+/** calculate the length of the input string*/
 	while (str[i] != '\0')
 	{
 		i++;
 		n++;
 	}
+/** increment n to account for the NULL terminator*/
 	n++;
-
+/** allocate memory for the duplicate string*/
 	ar = malloc(n * sizeof(char));
 	if (ar == NULL)
 	{
 		return (NULL);
 	}
-
+/** copy the contents of the input string to the duplicate string*/
 	for (i = 0 ; i < n ; i++)
 	{
 		ar[i] = str[i];
 	}
-
+/** return the pointer to the duplicate string*/
 	return (ar);
 }
--- stringhelper.c
+++ stringhelper.c
@@ -8,17 +8,19 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
-
+/** find the end of the destination string*/
 	while (dest[i])
 	{
 		i++;
 	}
+/** concatenate the source string and the destination string*/
 	while (src[j])
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+/** add NULL terminator and return pointer to new string*/
 	dest[i] = '\0';
 	return (dest);
 }
@@ -31,12 +33,14 @@ char *_strcat(char *dest, char *src)
 char *_strcpy(char *dest, char *src)
 {
 	int a = 0;
-
+/** copy characters from src to dest until NULL*/
 	while (*(src + a))
 	{
+/** copy characters and increment through*/
 		*(dest + a) = *(src + a);
 		a++;
 	}
+/** add NULL to mark end of string and return pointer to string*/
 	dest[a] = '\0';
 	return (dest);
 }
@@ -49,9 +53,10 @@ char *_strcpy(char *dest, char *src)
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, dif = 0;
-
+/** loop until NULL or characters dont match*/
 	while (s1[i] && s2[i])
 	{
+/** check if the characters of both strings are equal*/
 		if (s1[i] == s2[i])
 		{
 			i++;
@@ -59,10 +64,12 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 		{
+/** calculate difference in ASCII values*/
 			dif = s1[i] - s2[i];
 			break;
 		}
 	}
+/** return the difference inn ASCII values*/
 	return (dif);
 }
 /**
@@ -73,7 +80,7 @@ int _strcmp(char *s1, char *s2)
 int _strlen(char *s)
 {
 	int a = 0;
-
+/** loop until reaching the NULL terminator*/
 	while (*(s + a) != '\0')
 	{
 		a++;
@@ -90,9 +97,10 @@ int _strlen(char *s)
 int _strncmp(char *s1, char *s2, int n)
 {
 	int i = 0, dif = 0;
-
+/** loop until reaching the end of either strings or until strings are compared*/
 	while (s1[i] && s2[i] && (i < n))
 	{
+/** compare characters at the same index*/
 		if (s1[i] == s2[i])
 		{
 			i++;
@@ -100,9 +108,11 @@ int _strncmp(char *s1, char *s2, int n)
 		}
 		else
 		{
+/** calculate the difference between the characters*/
 			dif = s1[i] - s2[i];
 			break;
 		}
 	}
+/** return the difference between the characters*/
 	return (dif);
 }
